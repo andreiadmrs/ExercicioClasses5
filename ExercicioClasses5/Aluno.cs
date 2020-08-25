21bbdae244def29c1b55c83aa37c88536b59597b
Andreia de Moraes <andreiadmrs@gmail.com>
2020-08-24 21:56:31 -0300

    Exercício do curso de C#. Classes, atributos e métodos.

---
+++ ExercicioClasses5/Aluno.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExercicioClasses5
+{
+    class Aluno
+    {
+        public double PrimeiroTrimestre;
+        public double SegundoTrimestre;
+        public double TerceiroTrimestre;
+        public string Nome;
+
+
+        public double NotaFinal()
+        {
+            return PrimeiroTrimestre + SegundoTrimestre + TerceiroTrimestre;
+        }
+        public bool Aprovado()
+        {
+            if (NotaFinal() >= 60)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public double NotaRestante()
+        {
+            if (Aprovado())
+            {
+                return 0.0;
+            }
+            else
+            {
+                return 60.0 - NotaFinal();
+            }
+        }
+    }
+}
---
+++ ExercicioClasses5/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+namespace ExercicioClasses5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Fazer um programa para ler o nome de um aluno e as três notas que ele obteve nos três trimestres do ano
+            (primeiro trimestre vale 30 e o segundo e terceiro valem 35 cada). Ao final, mostrar qual a nota final do aluno no
+            ano. Dizer também se o aluno está APROVADO ou REPROVADO e, em caso negativo, quantos pontos faltam
+            para o aluno obter o mínimo para ser aprovado (que é 60 pontos). Você deve criar uma classe Aluno para resolver
+            este problema.*/
+
+            var aluno = new Aluno();
+
+            Console.WriteLine("Digite nome:");
+            aluno.Nome = Console.ReadLine();
+            Console.WriteLine("Digite primeira nota:");
+            aluno.PrimeiroTrimestre = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Digite segunda nota:");
+            aluno.SegundoTrimestre = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Digite terceira nota:");
+            aluno.TerceiroTrimestre = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Nota final: "+ aluno.NotaFinal().ToString("F2", CultureInfo.InvariantCulture));
+
+            if (aluno.Aprovado()== true)
+            {
+                Console.WriteLine("Aprovado");
+            }
+            else
+            {
+                Console.WriteLine("Falta " + aluno.NotaRestante().ToString("F2", CultureInfo.InvariantCulture) + " para aprovação.");
+            }
+
+        }
+    }
+}
